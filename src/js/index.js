685d838da55d4eda8e11742327fff690bd5a6748
ViviRodri2010 <146996438+ViviRodri2010@users.noreply.github.com>
2023-12-02 01:24:37 +0000

    Crear counter

--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,24 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(() => {
+    counter ++;
+    let units = Math.floor((counter / 1) % 10);
+    let tenths = Math.floor((counter / 10) % 10);
+    let hundreds = Math.floor((counter / 100) % 10);
+    let thousands = Math.floor((counter / 1000) % 10);
+    let tenthousands = Math.floor((counter / 10000) % 10);
+    let hundredthousands = Math.floor((counter / 100000) % 10);
+    ReactDOM.render((
+    <>
+      <div>
+        <h1>{hundredthousands}</h1> 
+        <h1>{tenthousands}</h1>
+        <h1>{thousands}</h1> 
+        <h1>{hundreds}</h1> 
+        <h1>{tenths}</h1>
+        <h1>{units}</h1>
+      </div>
+    </>), document.querySelector("#app"));
+}, 1000)
