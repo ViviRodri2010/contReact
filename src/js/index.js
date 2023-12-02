3d15fbc399465b7392406eb124c6698eb60157db
ViviRodri2010 <146996438+ViviRodri2010@users.noreply.github.com>
2023-12-01 20:57:24 -0500

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -43,22 +43,22 @@ setInterval(() => {
   if(update){
     counter ++;
   }
-    let units = Math.floor((counter / 1) % 10);
-    let tenths = Math.floor((counter / 10) % 10);
-    let hundreds = Math.floor((counter / 100) % 10);
-    let thousands = Math.floor((counter / 1000) % 10);
-    let tenthousands = Math.floor((counter / 10000) % 10);
-    let hundredthousands = Math.floor((counter / 100000) % 10);
-    ReactDOM.render((
-    <>
-      <div>
-        <h1>⏰</h1>
-        <h1>{hundredthousands}</h1> 
-        <h1>{tenthousands}</h1>
-        <h1>{thousands}</h1> 
-        <h1>{hundreds}</h1> 
-        <h1>{tenths}</h1>
-        <h1>{units}</h1>
-      </div>
-    </>), document.querySelector("#app"));
+  let units = Math.floor((counter / 1) % 10);
+  let tenths = Math.floor((counter / 10) % 10);
+  let hundreds = Math.floor((counter / 100) % 10);
+  let thousands = Math.floor((counter / 1000) % 10);
+  let tenthousands = Math.floor((counter / 10000) % 10);
+  let hundredthousands = Math.floor((counter / 100000) % 10);
+  ReactDOM.render((
+  <>
+    <div>
+      <h1>⏰</h1>
+      <h1>{hundredthousands}</h1> 
+      <h1>{tenthousands}</h1>
+      <h1>{thousands}</h1> 
+      <h1>{hundreds}</h1> 
+      <h1>{tenths}</h1>
+      <h1>{units}</h1>
+    </div>
+  </>), document.querySelector("#app"));
 }, 1000)
